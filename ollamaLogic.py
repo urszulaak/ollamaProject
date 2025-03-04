151fba775a57c89169443acd7d0282e03c793415
urszulaak <mala.urszula@gmail.com>
2025-03-04 19:45:30 +0100

    First Commit

---
+++ ollamaLogic.py
@@ -0,0 +1,13 @@
+import ollama
+
+
+content = input("Enter message: ")
+
+response = ollama.chat(
+    model="llama3.1:8b",
+    messages=[
+        {"role": "user", "content": content},
+    ],
+)
+
+print(response["message"]["content"])
