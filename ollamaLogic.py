c3fb8cad5c957bd964bb8059d69ef0da97e80ed2
urszulaak <mala.urszula@gmail.com>
2025-03-11 11:51:46 +0100

    add polish LLM model and delete saving voice to txt

--- ollamaLogic.py
+++ ollamaLogic.py
@@ -3,14 +3,16 @@ import vosk
 import pyaudio
 import json
 
-model_path = ""
-
 language = int(input("Choose Language (1-English, 2-Polish): "))
+
+model_ai=""
 
 if language == 1:
     model_path = "vosk-model-en-us-0.22-lgraph"
+    model_ai = "llama3.1:8b"
 elif language == 2:
     model_path = "vosk-model-small-pl-0.22"
+    model_ai = "SpeakLeash/bielik-11b-v2.3-instruct:Q4_K_M"
 else:
     print("Wrong language")
 
@@ -26,30 +28,26 @@ stream = p.open(format=pyaudio.paInt16,
 
 output_file_path = "recognized_text.txt"
 
-with open(output_file_path, "w") as output_file:
-    print("Listening for speech. Say 'Amen' to stop.")
-    while True:
-        data = stream.read(4096)
-        if rec.AcceptWaveform(data):
-            result = json.loads(rec.Result())
-            recognized_text = result['text']
-            
-            output_file.write(recognized_text + "\n")
-            print(recognized_text)
+print("Listening for speech. Say 'Stop' to stop.")
+while True:
+    data = stream.read(4096)
+    if rec.AcceptWaveform(data):
+        result = json.loads(rec.Result())
+        recognized_text = result['text']
+        
+        print(recognized_text)
 
-            if "amen" in recognized_text.lower():
-                print("Termination keyword detected. Stopping...")
-                break
+        if "stop" in recognized_text.lower():
+            print("Termination keyword detected. Stopping...")
+            break
 
 stream.stop_stream()
 stream.close()
 
 p.terminate()
 
-#content = input("Enter message: ")
-
 response = ollama.chat(
-    model="llama3.1:8b",
+    model=model_ai,
     messages=[
         {"role": "user", "content": recognized_text},
     ],
